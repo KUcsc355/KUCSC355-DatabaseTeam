ab694a3e80c9e10aca8f5ea3f5a83d25ab8c164c
DJNUMA <rickkubik@yahoo.com>
2017-10-26 10:24:26 -0400

    Update eventCreate.php

--- eventCreate.php
+++ eventCreate.php
@@ -3,9 +3,9 @@
 //Due Date:	April 10th 2017
 
 	include("connect.php");
-	function createEvent($id, $name, $speaker, $address, $date, $fee, $description){
+	function createEvent($eName, $speaker, $street1, $street2, $city, $state, $zip, $date, $fee, $description){
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			$sql = "INSERT INTO Event (id, name, speaker, address, date, fee, description) VALUES ('$id','$name', '$speaker', '$address', '$date','$fee', '$description')";
+			$sql = "INSERT INTO Event (eName, speaker, street1, street2, city, state, zip, date, fee, description) VALUES ('$id','$name', '$speaker', '$street1', '$street2', '$city', '$state', '$zip', '$date','$fee', '$description')";
 			$db->query($sql);
 			return(echo("Event Created"));
 		}
