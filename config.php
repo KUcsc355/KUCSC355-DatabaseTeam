545f592d3e9a83095d775da8f2bb910e732040a2
DJNUMA <rickkubik@yahoo.com>
2017-10-26 10:04:26 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 Config.php
 -->
 <?php
-	$DB_HOST = 'localhost';
-	$DB_USER = 'rkubi507';
-	$DB_PASS = 'w3sAPhUr';
-	$DB_NAME = 'bookstore_rkubi507';
+	$DB_HOST = 'http://csc355-db-group.cfwk8ozvku9e.us-east-2.rds.amazonaws.com/';
+	$DB_USER = 'DB_Group';
+	$DB_PASS = 'Starwars001';
+	$DB_NAME = 'csc355-db-group';
 	$db = new mysqli($DB_HOST,  $DB_USER, $DB_PASS, $DB_NAME);
 ?>
